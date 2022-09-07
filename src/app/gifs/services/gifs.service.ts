7e5a4701a671449df102207367d59d384908a927
Edsonjsf <edsonjsf1990@gmail.com>
2022-09-07 17:08:52 -0400

    Peticiones http con HttpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey: string = 'bFAARjufg25n3oCgy268kCSuaHYcGMsi';
   private _historial: string[] = [];
 
   public get historial(): string[] {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient) {}
 
   buscarGifs(query: string = '') {
     if (!query) return;
@@ -16,10 +20,20 @@ export class GifsService {
     query = query.trim().toLocaleLowerCase();
     if (this._historial.includes(query)) return;
 
+    this.updateHistorial(query);
+
+    this.http
+      .get(
+        `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  private updateHistorial(query: string) {
     this._historial.unshift(query);
     this.historialMax(10);
-
-    console.log(this._historial);
   }
 
   private historialMax(maxCantidad: number) {
