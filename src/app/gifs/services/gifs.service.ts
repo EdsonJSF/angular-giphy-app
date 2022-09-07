46e53a48b669e37b5ee712c0f69f5e7ba5232af0
Edsonjsf <edsonjsf1990@gmail.com>
2022-09-07 16:24:51 -0400

    Control del historial de las busquedas

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,8 +10,19 @@ export class GifsService {
     return [...this._historial];
   }
 
-  buscarGifs(query: string) {
+  buscarGifs(query: string = '') {
+    if (!query) return;
+
+    query = query.trim().toLocaleLowerCase();
+    if (this._historial.includes(query)) return;
+
     this._historial.unshift(query);
+    this.historialMax(10);
+
     console.log(this._historial);
+  }
+
+  private historialMax(maxCantidad: number) {
+    this._historial = this._historial.slice(0, maxCantidad);
   }
 }
